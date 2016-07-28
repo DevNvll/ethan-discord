a3e45abd95a3559d2fb7a70da5a1db051ba521a9
Henrick Mello <hmartins505@gmail.com>
2016-07-28 20:29:19 -0300

    Add files via upload

---
+++ commands/steamtime.js
@@ -0,0 +1,49 @@
+const request = require('axios');
+const _ = require('lodash');
+const cfg = require('../botCfg.json');
+
+module.exports = {
+  name: 'Steam Time',
+  description: 'Get user\'s played hours on steam (owned games only)',
+  command: "!steamtime <steamid>",
+  trigger: '!steamtime',
+  permissions: [],
+  run: (bot, channel, message) => {
+    resolveId(message.content.match(/!steamtime (.*)/i)[1], (time) => {
+      time ? bot.reply(message, "Total hours played: " + time) : bot.reply(message, 'Profile is private or doesn\'t exist');
+    });
+  }
+}
+
+
+let getTime = (userid, callback) => {
+  let hours = 0;
+  request
+  .get('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key='+ cfg.steamKey +'&steamid='+ userid +'&format=json')
+  .then((res) => {
+    if(_.some(res.data.response)) {
+      for(let game in res.data.response.games) {
+        hours = hours + parseInt(res.data.response.games[game].playtime_forever/60);
+      }
+      callback(hours);
+    } else {
+      callback()
+    }
+
+  });
+}
+
+const resolveId = (id, callback) => {
+  if(id.match(/^7656119[0-9]{10}$/i)) {
+    getTime(id, (time) => {
+      callback(time);
+    });
+  } else {
+    request.get('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key='+ cfg.steamKey +'&vanityurl='+ id)
+    .then((res) => {
+      getTime(res.data.response.steamid, (time) => {
+        callback(time);
+      });
+    })
+  }
+}
